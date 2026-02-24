c3b1a6ae6f8c823414b7671d86e1cdc8baa781f8
ayushyadav1355-coder <ayushyadav1355@gmail.com>
2026-02-24 23:01:43 +0530

    variable commit

---
+++ note2.js
@@ -0,0 +1,27 @@
+// Type of variables
+// There are 7 types of variables in JavaScript:
+// 1. String: A sequence of characters enclosed in single or double quotes. Example: "Hello, World!"
+// 2. Number: Represents both integer and floating-point numbers. Example: 42, 3.14 
+// 3. Boolean: Represents a logical entity that can have two values: true or false. Example: true, false
+// 4. Null: Represents the intentional absence of any object value. Example: null
+// 5. Undefined: Represents a variable that has been declared but has not been assigned a value. Example: undefined
+// 6. Symbol: Represents a unique identifier. Example: Symbol('description')
+// 7. Object: Represents a collection of properties and methods. Example: { name: "Ayush", age: 22 }
+
+
+
+
+//Data types in JS:
+let name = "ayush";        // String
+let age = 19;              // Number
+let isStudent = true;      // Boolean
+let score;                 // Undefined
+let car = null;            // Null
+let person = { name: "ayush", age: 25 }; // Object
+console.log(typeof name); // string
+console.log(typeof age);  // number
+
+console.log(typeof isStudent);  // boolean
+console.log(typeof score);      // undefined
+console.log(typeof car);        // object
+console.log(typeof person);     // object
